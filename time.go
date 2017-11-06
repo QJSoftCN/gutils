fe9a96e7b79887ccc4e1ea1f3a833af80031b617
tpri-pwh <tpri-pwh@qq.com>
2017-11-07 05:14:41 +0800

    add togotimefmt

--- time.go
+++ time.go
@@ -1,6 +1,9 @@
 package gutils
 
-import "time"
+import (
+	"time"
+	"strings"
+)
 
 const (
 	TF_ToSecond        = "2006-01-02 15:04:05"
@@ -15,6 +18,15 @@ func FormatToSecondForFileName(time time.Time)string{
 	return time.Format(TF_ToSecondForName)
 }
 
+func ToGoTimeFmt(f string) string {
+	f = strings.Replace(f, "yyyy", "2006", 1)
+	f = strings.Replace(f, "MM", "01", 1)
+	f = strings.Replace(f, "dd", "02", 1)
+	f = strings.Replace(f, "hh", "15", 1)
+	f = strings.Replace(f, "mm", "04", 1)
+	f = strings.Replace(f, "ss", "05", 1)
+	return f
+}
 
 
 
