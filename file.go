887800bef1c0235d9e9d37d412cd306107a51d32
tpri-pwh <tpri-pwh@qq.com>
2017-11-09 06:22:54 +0800

    add excel axis calc lib

--- file.go
+++ file.go
@@ -3,6 +3,7 @@ package gutils
 import (
 	"os"
 	"log"
+	"io"
 )
 
 func PathExists(path string) bool {
@@ -26,6 +27,38 @@ func Dir(dir string) string {
 		log.Println(err)
 	}
 	return dir
+}
+
+//copy dir or file
+func CopyFile(source, dest string) bool {
+	if source == "" || dest == "" {
+		log.Println("source or dest is null")
+		return false
+	}
+
+	source_open, err := os.Open(source)
+
+	if err != nil {
+		log.Println(err.Error())
+		return false
+	}
+	defer source_open.Close()
+
+	dest_open, err := os.OpenFile(dest, os.O_CREATE|os.O_WRONLY, 644)
+	if err != nil {
+		log.Println(err.Error())
+		return false
+	}
+	//养成好习惯。操作文件时候记得添加 defer 关闭文件资源代码
+	defer dest_open.Close()
+	//进行数据拷贝
+	_, copy_err := io.Copy(dest_open, source_open)
+	if copy_err != nil {
+		log.Println(copy_err.Error())
+		return false
+	} else {
+		return true
+	}
 }
 
 
