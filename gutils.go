f3ae8fcb1a119edc7378aad75174904b7084de75
tpri-pwh <tpri-pwh@qq.com>
2017-11-10 09:44:15 +0800

    move conf to confs

--- gutils.go
+++ gutils.go
@@ -8,21 +8,5 @@ import (
 )
 
 func init() {
-	all = make(map[string](map[string]interface{}))
 
-	files, err := ioutil.ReadDir(dir_conf)
-	if err != nil {
-		log.Fatal(err)
-		return
-	}
-
-	for index, file := range files {
-		log.Println("read No.", index+1, " confige file name:", file.Name())
-		switch strings.ToLower(path.Ext(file.Name())) {
-		case json_file:
-			readConfByJson(file.Name())
-		default:
-			readConfByJson(file.Name())
-		}
-	}
 }
